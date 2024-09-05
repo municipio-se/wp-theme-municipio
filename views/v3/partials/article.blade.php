bbfd1503f2ac6b33da46c5e05d73165bda13fe7e
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2024-09-05 10:40:46 +0200

    fix: move readspeaker id to article.blade

--- views/v3/partials/article.blade.php
+++ views/v3/partials/article.blade.php
@@ -1,3 +1,4 @@
+<div id="readspeaker-read">
 <article class="c-article c-article--readable-width s-article u-clearfix" id="article" {!! !empty($postLanguage) ? 'lang="' . $postLanguage . '"' : '' !!}>
 
     <!-- Title -->
@@ -87,3 +88,4 @@
     @endsection
 
 </article>
+</div>
--- views/v3/templates/master.blade.php
+++ views/v3/templates/master.blade.php
@@ -81,7 +81,7 @@
                     @endif
 
                     <!--  Main content padder -->
-                    <div id="readspeaker-read"
+                    <div
                         class="u-padding__x--{{ $mainContentPadding['md'] }}@lg u-padding__x--{{ $mainContentPadding['lg'] }}@lg u-padding__x--{{ $mainContentPadding['lg'] }}@xl u-margin__bottom--12">
                         <div class="o-grid o-grid--nowrap@lg o-grid--nowrap@xl">
 
