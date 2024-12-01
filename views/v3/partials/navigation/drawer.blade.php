6018aa92f2e8791c45576c4e1945c54d15aa8f94
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2024-12-01 17:27:13 +0100

    fix: remove drawer classes for correct styles (#14)

--- views/v3/partials/navigation/drawer.blade.php
+++ views/v3/partials/navigation/drawer.blade.php
@@ -19,7 +19,7 @@
     'id' => 'drawer',
     'attributeList' => ['data-move-to' => 'body', 'data-js-toggle-item' => 'drawer'],
     'classList' => [
-        'c-drawer--' . (!empty($mobileMenuItems)&&!empty($mobileMenuSecondaryItems) ? 'duotone' : 'monotone'),
+        'c-drawer--monotone',
         's-drawer-menu'
     ],
     'label' => $lang->close,
@@ -34,12 +34,12 @@
             )
     @endslot
 
-    @if (!empty($mobileMenuItems)||!empty($mobileMenuSecondaryItems)) 
+    @if (!empty($mobileMenuItems)||!empty($mobileMenuSecondaryItems))
     @slot('menu')
         @includeIf(
-            'partials.navigation.mobile', 
+            'partials.navigation.mobile',
                 [
-                    'menuItems' => $mobileMenuItems, 
+                    'menuItems' => $mobileMenuItems,
                     'classList' => [
                         'c-nav--drawer',
                         'site-nav-mobile__primary',
@@ -47,7 +47,7 @@
                         's-nav-drawer-primary'
                     ]
                 ]
-            )  
+            )
 
                 {{-- No ajax in wp-menus, thus not in its own file --}}
 
@@ -57,7 +57,6 @@
                         'c-nav--drawer',
                         'site-nav-mobile__secondary',
                         's-nav-drawer',
-                        's-nav-drawer-secondary'
                     ],
                     'items' => $mobileMenuSecondaryItems,
                     'direction' => 'vertical',
@@ -65,11 +64,11 @@
                     'height' => 'sm',
                     'expandLabel' => $lang->expand
                 ])
-            @endnav  
+            @endnav
         @endslot
       @else
       {{-- No menu items found --}}
       @endif
 
-@enddrawer  
+@enddrawer
 @endif
