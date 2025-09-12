50d4823e8d5beeba21216e938ba7cd2157c6548d
jeanfredrik <jeanfredrik@gmail.com>
2025-09-12 16:15:11 +0200

    Add "Municipio/allowCustomCode" filter

--- library/Theme/CustomCodeInput.php
+++ library/Theme/CustomCodeInput.php
@@ -6,7 +6,8 @@ class CustomCodeInput
 {
     public function __construct()
     {
-        if (!is_admin()) {
+        $allow_custom_code = apply_filters('Municipio/allowCustomCode', true);
+        if (!is_admin() && $allow_custom_code) {
             add_action('wp_head', array($this, 'printCssCode'), 999);
             add_action('wp_footer', array($this, 'printJsCode'), 999);
             add_action('wp_head', array($this, 'headerScripts'), 999);
