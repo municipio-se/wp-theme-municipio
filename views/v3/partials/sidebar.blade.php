f2ffe30f3848ae21e053dcb55e2e6f1f708a8e6b
jeanfredrik <jeanfredrik@gmail.com>
2025-09-23 15:45:16 +0200

    Add "Municipio/views/partials/sidebar/classes" filter

--- views/v3/partials/sidebar.blade.php
+++ views/v3/partials/sidebar.blade.php
@@ -1,8 +1,13 @@
+@php
+    $classes ??= [];
+    $classes = (array) ($classes ?: []);
+    $classes = apply_filters('Municipio/views/partials/sidebar/classes', $classes, $id);
+@endphp
 @if(isset($id) && $id)
     @section('sidebar.' . $id . '.before')@show
     @if (is_active_sidebar($id))
         @section('sidebar.active.' . $id . '.before')@show
-        <div id="sidebar-{{$id}}" class="s-sidebar s-sidebar-{{$id}} sidebar-{{$id}} {{isset($classes) ? is_array($classes) ? implode(' ', $classes) : $classes : ''}}">
+        <div id="sidebar-{{$id}}" class="s-sidebar s-sidebar-{{$id}} sidebar-{{$id}} {{ implode(' ', $classes) }}">
             @php dynamic_sidebar($id); @endphp
         </div>
         @section('sidebar.active.' . $id . '.after')@show
