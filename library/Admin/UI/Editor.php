f019b3f7f2e15c32fbb97e44ce1313be838aa985
Johan Westin <johan.westin@whitespace.se>
2025-11-19 15:26:45 +0100

    Disable advanced HTML tags in editor by default
    Change Municipio/Admin/AllowAdvancedHtmlTags filter default from true
    to false to reduce XSS attack surface (secure by default principle).

--- library/Admin/UI/Editor.php
+++ library/Admin/UI/Editor.php
@@ -27,7 +27,7 @@ class Editor
         $this->pricons();
 
         // Filters
-        $allow_advanced_html_tags = apply_filters('Municipio/Admin/AllowAdvancedHtmlTags', true);
+        $allow_advanced_html_tags = apply_filters('Municipio/Admin/AllowAdvancedHtmlTags', false);
         if($allow_advanced_html_tags) {
             // Allow specific html tags for editors
             add_filter('tiny_mce_before_init', array($this, 'allowedHtmlTags'));
